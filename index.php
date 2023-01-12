edfc9f7934e181974fbe36cb7aae3ba49335d1de
Milan Petrovic <milanmushin@gmail.com>
2023-01-12 22:47:07 +0100

    Small while refresher

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <h1><?php bloginfo('name'); ?></h1>
 <p><?php bloginfo('description'); ?></p>
+
+    <?php
+
+        $names = array('Dakillzor', 'Skill', 'AEG');
+
+        $count = 0;
+
+        while($count < count($names)) {
+            echo "<li>Hi, my name is $names[$count]</li>";
+            $count++;
+        }
+
+    ?>
